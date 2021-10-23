06c1daf6c0e00d7d10c05b7b02711700852b6758
Neil-Chorgi <neilchorgi@gmail.com>
2021-10-23 17:56:05 +0100

    Database set up

--- restaurant/hotel/models.py
+++ restaurant/hotel/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Dish(models.Model):
     dish_text = models.CharField(max_length=200)
-    description_text = models.CharField(max_length = 200)
+    description_text = models.CharField(max_length = 1000)
     price = models.DecimalField(max_digits = 12,decimal_places = 2,default = 0)
--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'polls',
+        'NAME': 'hotel',
         'USER': 'postgres',
         'PASSWORD': 'NIvi2008',
         'HOST': 'localhost',
