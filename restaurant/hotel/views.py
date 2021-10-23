c3da1aee5d3fc0cba2d142be4ec85a036ba557ca
Neil-Chorgi <neilchorgi@gmail.com>
2021-10-23 20:07:48 +0100

    Clened the output of the menu items

--- restaurant/hotel/views.py
+++ restaurant/hotel/views.py
@@ -11,5 +11,5 @@ def menu(request, dish_id):
     #return HttpResponse("You're looking at dish %s." % dish_id)
     menu_list = Dish.objects.all()
     d = menu_list[dish_id]
-    output = d.dish_text,"\n",d.description_text,"\nPrice: £"+str(d.price)
+    output = d.dish_text+", "+d.description_text+". Price: £"+str(d.price)
     return HttpResponse(output)
