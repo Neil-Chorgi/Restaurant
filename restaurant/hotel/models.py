f82096564199cfdf0fe27fd3e4d413e14e89f13b
Neil-Chorgi <neilchorgi@gmail.com>
2021-10-23 17:03:10 +0100

    Dish item class created

--- restaurant/hotel/models.py
+++ restaurant/hotel/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Dish(models.Model):
+    dish_text = models.CharField(max_length=200)
+    description_text = models.CharField(max_length = 200)
+    price = models.DecimalField(default=0)
