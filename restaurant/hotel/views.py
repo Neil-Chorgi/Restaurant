9e8d244a97a41594d17d7bf6c99a87240b533451
Neil-Chorgi <neilchorgi@gmail.com>
2021-10-23 16:43:45 +0100

    gives output

--- restaurant/hotel/views.py
+++ restaurant/hotel/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    #runs if url = ' ' 
     return HttpResponse("You are at the Restaurant.")
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #redirects /hotel to urls.py in the hotel(app)
     path('hotel/', include('hotel.urls')),
 ]
