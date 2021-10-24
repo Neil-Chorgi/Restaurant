fdd7608974a0d0f6e196b8b9550d8476782ddf43
Neil-Chorgi <neilchorgi@gmail.com>
2021-10-24 09:22:09 +0100

    Indexing error fixed

--- restaurant/hotel/views.py
+++ restaurant/hotel/views.py
@@ -5,13 +5,22 @@ from .models import Dish
 # Create your views here.
 
 def index(request):
+    '''
     #runs if url = ' ' 
     return HttpResponse("You are at the Restaurant.")
-
+    '''
+    menu_list = Dish.objects.all()
+    template = loader.get_template('hotel/index.html')
+    context = {
+        'menu_list': menu_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
+    
 def menu(request, dish_id):
     #return HttpResponse("You're looking at dish %s." % dish_id)
     menu_list = Dish.objects.all()
     
-    d = menu_list[dish_id]
+    d = menu_list[dish_id - 1]
     output = d.dish_text+", "+d.description_text+". Price: £"+str(d.price)
     return HttpResponse(output)
