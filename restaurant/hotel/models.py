65898b73f797b2ad85c7ddacbbe24a6e786c4bf6
Neil-Chorgi <neilchorgi@gmail.com>
2021-10-24 10:18:46 +0100

    Added a dish type

--- restaurant/hotel/models.py
+++ restaurant/hotel/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-
 # Create your models here.
 class Dish(models.Model):
+    #Variables
+    DISH_TYPE = (
+        ('STARTER', "Starter"),
+        ('MAIN', "Main dish"),
+        ('DESSERT', "Dessert"),
+    )
     dish_text = models.CharField(max_length=200)
     description_text = models.CharField(max_length = 1000)
     price = models.DecimalField(max_digits = 12,decimal_places = 2,default = 0)
+    type_of_dish = models.CharField(max_length = 20,choices = DISH_TYPE,default = 'MAIN')
     
     def __str__(self):
         return self.dish_text
