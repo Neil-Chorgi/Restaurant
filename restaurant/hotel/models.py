7fbd8e9b20696204ab54313e43ad051ca0478a58
Neil-Chorgi <neilchorgi@gmail.com>
2021-10-24 11:00:23 +0100

    Drinks type added

--- restaurant/hotel/models.py
+++ restaurant/hotel/models.py
@@ -6,11 +6,13 @@ class Dish(models.Model):
         ('STARTER', "Starter"),
         ('MAIN', "Main dish"),
         ('DESSERT', "Dessert"),
+        ('DRINK',"Drink"),
+        
     )
     dish_text = models.CharField(max_length=200)
     description_text = models.CharField(max_length = 1000)
     price = models.DecimalField(max_digits = 12,decimal_places = 2,default = 0)
-    type_of_dish = models.CharField(max_length = 20,choices = DISH_TYPE,default = 'MAIN')
+    type_of_dish = models.CharField(max_length = 50,choices = DISH_TYPE,default = 'MAIN')
     
     def __str__(self):
         return self.dish_text
